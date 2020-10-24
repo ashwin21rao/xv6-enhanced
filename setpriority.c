3019bc85c7a78e5ce3ee6ca3093286ab09dd0d55
Ashwin Rao <ashwinisawesome007@gmail.com>
2020-10-24 21:25:30 +0530

    Add set_priority syscall

---
+++ setpriority.c
@@ -0,0 +1,19 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+    if(argc < 2)
+    {
+        printf(2, "Not enough arguments\n");
+        exit();
+    }
+    if(argc > 3)
+    {
+        printf(2, "Too many arguments\n");
+        exit();
+    }
+    set_priority(atoi(argv[1]), atoi(argv[2]));
+    exit();
+}
